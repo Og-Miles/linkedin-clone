9d29689211b1f82d1c7ef51a7ca48e655d631c53
Og-Miles <emmanuelogbuji5@gmail.com>
2023-07-24 22:58:11 +0100

    First commit

---
+++ src/Sidebar.jsx
@@ -0,0 +1,29 @@
+import { Avatar } from "@mui/material"
+import "./Sidebar.css"
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+        <div className="sidebar__">
+            <img src="" alt="" />
+            <Avatar className="sidebar__avater" />
+            <h2>Emmanuel Ogbuji</h2>
+            <h4>---.miles@gmail.com</h4>
+        </div>
+
+        <div className="sidebar__stats">
+            <div className="sidebar__stat">
+                
+            </div>
+            
+            <div className="sidebar__stat">
+
+            </div>
+        </div>
+    </div>
+
+
+  )
+}
+
+export default Sidebar
