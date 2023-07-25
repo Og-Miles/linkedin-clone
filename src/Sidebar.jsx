b2c77c0bd8030d8973e889da692d6826d1ac63f3
Og-Miles <emmanuelogbuji5@gmail.com>
2023-07-25 01:34:23 +0100

    Second commit

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -2,23 +2,41 @@ import { Avatar } from "@mui/material"
 import "./Sidebar.css"
 
 function Sidebar() {
+    const recentItem =(hash)=>(
+        <div className="sidebar__recentItem">
+            <span className="sidebar__hash">#</span>
+            <p>{hash}</p>
+        </div>
+    )
+
   return (
     <div className='sidebar'>
-        <div className="sidebar__">
-            <img src="" alt="" />
-            <Avatar className="sidebar__avater" />
+        <div className="sidebar__top">
+            <img src="https://unsplash.com/photos/nY14Fs8pxT8/download?force=true&w=640" alt="" />
+            <Avatar className="sidebar__avatar" />
             <h2>Emmanuel Ogbuji</h2>
-            <h4>---.miles@gmail.com</h4>
+            <h4>ogbuji.emmanuel@gmail.com</h4>
         </div>
 
         <div className="sidebar__stats">
             <div className="sidebar__stat">
-                
+                <p>Who viewed you</p>    
+                <p className="sidebar__statNumber">2,637</p>    
             </div>
             
             <div className="sidebar__stat">
-
+                <p>Views on post</p>    
+                <p className="sidebar__statNumber">2,129</p>  
             </div>
+        </div>
+
+        <div className="sidebar__bottom">
+            <p>Recent</p>
+            {recentItem('reactjs')}
+            {recentItem('ui/ux')}
+            {recentItem('programing')}
+            {recentItem('softwareengineering')}
+            {recentItem('web development')}
         </div>
     </div>
 
