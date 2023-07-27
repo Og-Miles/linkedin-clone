76fee974e8a00a2b2600a33d9e3d8e09219fd570
Og-Miles <emmanuelogbuji5@gmail.com>
2023-07-27 23:23:42 +0100

    Third commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,32 @@ import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/counter/userSlice';
+import Login from './Login';
 
 
 function App() {
+    const user = useSelector(selectUser);
+
   return (
     <div className="app">
-        <Header />
 
-      {/* App body */}
-      <div className="app__body">
-        <Sidebar/>
-        <Feed/>
-      {/* Widgets */}
-      </div>
+        {!user ? (
+          <Login /> 
+          )
+          : (
+          <>
+            <Header />
+            
+            <div className="app__body">
+              <Sidebar />
+              <Feed />
+              {/* Widgets */}
+            </div>
+          </>
+
+        )}
     </div>
   );
 }
