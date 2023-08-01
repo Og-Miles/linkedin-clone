ba38a652fab396a0e1770acfe799e003d924dff2
Og-Miles <emmanuelogbuji5@gmail.com>
2023-08-01 20:22:49 +0100

    Login formatted

--- src/Login.js
+++ src/Login.js
@@ -1,74 +1,90 @@
-import './Login.css';
-import { auth } from './firebase';
-import logo from './assets/logo.png'
-import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { login } from './features/counter/userSlice';
-
+import "./Login.css";
+import { auth } from "./firebase";
+import logo from "./assets/logo.png";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { login } from "./features/counter/userSlice";
 
 function Login() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayPic, setDisplayPic] = useState("");
+  const dispatch = useDispatch();
 
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [displayPic, setDisplayPic] = useState('');
-  const dispatch = useDispatch(); 
-
-    const loginToApp = (e) => {
-        e.preventDefault();
+  const loginToApp = (e) => {
+    e.preventDefault();
+  };
+  const register = () => {
+    if (!name) {
+      return alert("Error: Full Name Required!");
     }
-    const register = () => {
-      if (!name) {
-        return alert('Error: Full Name Required!');
-      }
 
-      auth.createUserWithEmailAndPassword(email, password).then((userAuth) => {
-        userAuth.user.updateProfile({
-          displayName: name,
-          photoURL: displayPic,
-        }).then(() => {
-          dispatch(login({
-            email: userAuth.user.email,
-            uid: userAuth.user.uid,
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userAuth) => {
+        userAuth.user
+          .updateProfile({
             displayName: name,
             photoURL: displayPic,
-          }))
-        })
+          })
+          .then(() => {
+            dispatch(
+              login({
+                email: userAuth.user.email,
+                uid: userAuth.user.uid,
+                displayName: name,
+                photoURL: displayPic,
+              })
+            );
+          });
       })
-
-    };
+      .catch((error) => alert(error.message));
+  };
   return (
-    <div className='login'>
-       <img src={logo} alt="linkedin" />
+    <div className="login">
+      <img src={logo} alt="linkedin" />
 
-        <form>
+      <form>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Full name (required if registered)"
+          type="text"
+        />
 
-            <input 
-            value={name} 
-            onChange={(e) => setName(e.target.value)} placeholder='Full name (required if registered)' type="text" />
+        <input
+          value={displayPic}
+          onChange={(e) => setDisplayPic(e.target.value)}
+          placeholder="profile picture Url"
+          type="text"
+        />
 
-            <input value={displayPic} 
-            onChange={(e) => setDisplayPic (e.target.value)} placeholder='profile picture Url' type="text" />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          type="text"
+        />
 
-            <input 
-            value={email} 
-            onChange={(e) => setEmail (e.target.value)} placeholder='Email' type="text" />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Enter your password"
+          type="password"
+        />
 
-            <input 
-            value={password} 
-            onChange={(e)=> setPassword(e.target.value)} placeholder='Enter your password' type="password" />
-
-            <button>Sign in</button>
-
-        </form>
-        <p>Not a member? 
-
-            <span className='login__register' onClick={register}> Register Now</span>
-        </p>
-
-        
+        <button>Sign in</button>
+      </form>
+      <p>
+        Not a member?
+        <span className="login__register" onClick={register}>
+          {" "}
+          Register Now
+        </span>
+      </p>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
